6550beb307f39e52d32b476f273c255d7b4f008c
thomashalo11 <thomastresca11@gmail.com>
2024-01-26 13:48:08 +0100

    Spettacoli cinematografici

---
+++ spettacoli_cinematografici/film.php
@@ -0,0 +1,41 @@
+<?php
+// Connessione al database
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "film";
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+// Controllo di avvennuta connessione
+if($conn -> connect_error)
+    die("Errore di connessione");
+?>
+
+<html>
+<head>
+    <title>Elenco film</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body style="font-family: Verdana">
+    <h1>Elenco film: </h1>
+
+    <?php
+    // Impostazione query da eseguire
+    $sql = "SELECT * FROM film";
+    //echo $sql | Permette di verificarne la correttezza
+    $tab = $myconn -> query($sql);
+
+    while($row = $tab -> fetch_array(MYSQLI_NUM)) {
+        // Visualizzato i valori presenti nella prima colonna $row['0']
+        echo "Titolo: ".$row['0']."- Regista: ".$row['2']."<br>";
+    }
+    ?>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
