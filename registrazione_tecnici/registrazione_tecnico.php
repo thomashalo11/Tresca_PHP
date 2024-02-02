dabbf2e2ab609778371fb709c90ef6da53ba46ed
thomashalo11 <thomastresca11@gmail.com>
2024-02-02 13:51:27 +0100

    Registrazioni

--- registrazione_tecnici/registrazione_tecnico.php
+++ registrazione_tecnici/registrazione_tecnico.php
@@ -9,31 +9,28 @@ $conn = new mysqli($hostname, $username, $password, $dbname);
 if($conn -> connect_error)
     die("Errore di connessione");
 // Recupero e assegna a 2 variabili username e password inviate dalla pagina registrazione_cliente.php
-$username = $_POST["username"];
-$password = $_POST["password"];
+$specializzazione = $_POST["specializzazione"];
 $cognome = $_POST["cognome"];
 $nome = $_POST["nome"];
-// Query per verificare se esiste già un cliente con lo stesso username
-$sql = "SELECT * FROM clienti WHERE username = '{$username}'";
-echo($sql);
 // Prima di procedere verificare la correttezza sintattica del comando assegnato alla variabile $sql con echo($sql)
+$sql = "SELECT * FROM tecnici";
 $result = $conn -> query($sql);
 if($result -> num_rows > 0) {
     // Credenziali valide
     // Reindirizzamento a una pagina inserimento.html
-    header("Location: registrazione_cliente.html");
+    header("Location: registrazione_tecnico.html");
     exit();
 }else {
     // Creazione query di comando sql INSERT INTO
-    $sql = "INSERT INTO clienti (nome, cognome, username, password)";
-    $sql.= "VALUES ('{$nome}','{$cognome}','{$username}','{$password}');";
+    $sql = "INSERT INTO tecnici (nome, cognome, specializzazione)";
+    $sql.= "VALUES ('{$nome}','{$cognome}','{$specializzazione}');";
 
-    $result = $conn -> query($sql) or die ("Query inserimento cliente fallita");
+    $result = $conn -> query($sql) or die ("Query inserimento tecnico fallita");
     // Visualizzazione tabella database aggiornata
-    $sql = "SELECT codice, cognome, nome from clienti";
+    $sql = "SELECT codice, cognome, nome from tecnici";
     $result = $conn -> query($sql) or die ("Query fallita");
     foreach($result as $riga) {
-        echo "<h3>Inserimento cliente {$cognome} {$nome} effettuato </h3>";
+        echo "<h3>Inserimento tecnico {$cognome} {$nome} effettuato </h3>";
         echo "<td>{$riga['cognome']}</td>";
         echo "<td>{$riga['nome']}</td>";
     }
